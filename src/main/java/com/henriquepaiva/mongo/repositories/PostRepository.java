9cc326429a55ec13b9cb06f0ed507cd9a6a55b6d
Henrique Paiva <josehenriquedsp@gmail.com>
2021-07-23 19:53:51 -0300

    Query with multiple criteria

--- src/main/java/com/henriquepaiva/mongo/repositories/PostRepository.java
+++ src/main/java/com/henriquepaiva/mongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.henriquepaiva.mongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -12,7 +13,10 @@ import com.henriquepaiva.mongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
-	List<Post> findByTitle(String text);
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } , {date: {$gte: ?1} } , {date: {$lte: ?2} } ] } }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
