257943de203d0129538f362e97667e455f349a23
Henrique Paiva <josehenriquedsp@gmail.com>
2021-07-23 11:55:47 -0300

    Getting users by Id

---
+++ src/main/java/com/henriquepaiva/mongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.henriquepaiva.mongo.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.henriquepaiva.mongo.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		String error = "Object not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
--- src/main/java/com/henriquepaiva/mongo/services/UserService.java
+++ src/main/java/com/henriquepaiva/mongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.henriquepaiva.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.henriquepaiva.mongo.domain.User;
 import com.henriquepaiva.mongo.repositories.UserRepository;
+import com.henriquepaiva.mongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,22 @@ public class UserService {
 		return repo.findAll();
 	}
 	
+	//Código na versão antiga do Springboot
+	
+	/*
+	public User findById(String id) {
+		User user = repo.findOne(id);
+		if(user == null) {
+			throw new ObjectNotFoundException("Object not found!");
+		}
+		return user;
+	}
+	*/
+	
+	//Código atualizado
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
 }
